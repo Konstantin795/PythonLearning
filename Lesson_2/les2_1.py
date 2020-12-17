1b0392d4b79715af16a5a10477d7a40b0d4903b6
Konstantin795 <6006974@gmail.com>
2020-12-18 04:10:30 +0500

    Lesson 2 Branch

--- Lesson_2/les2_1.py
+++ Lesson_2/les2_1.py
@@ -1,16 +1,39 @@
 """
-
-str
-int
+неизменяемые типы
 float
+int
 bool
 
-none
+
+None
+
+
+str
+tuple
 
 list
-tuple
 dict
 set
 
 """
 
+some = "kajsgfdhlaksjaskdjfaksdfmlaskdfmj"
+
+# for char in some:
+#     print(char)
+#
+# idx = 0
+#
+# while idx <len(some):
+#     item = some[idx]
+#     print(item)
+#     idx += 1
+
+some_list = [1, 2, 3, "some string", True, 22.3]
+
+some_dict = {
+    "one": 1652,
+    'two': 357336,
+    'user_name': 'some_name',
+    'user_age': 22,
+}
